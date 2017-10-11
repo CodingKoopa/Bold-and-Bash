639f5e15cf33de0f2925e27252d53254d1672bd0
TheKoopaKingdom <thekoopakingdom@gmail.com>
2017-10-11 17:09:32 -0400

    Fixed quote URLs.

--- Source/Commands/RandomQuote.js
+++ Source/Commands/RandomQuote.js
@@ -9,7 +9,14 @@ const DESCRIPTION = `Prints a random quote from the list.`;
 const callback = (message) =>
 {
   const quote = app.quotes[common.GetRandomNumber(0, app.quotes.length - 1)];
-  message.reply(`"${quote.quote_text}" - ${quote.username} (<@${quote.id}>)`);
+  var quote_text;
+  // If it doesn't have any spaces, don't include quote marks. Useful for URLs that have broken
+  // previews when quoted.
+  if (quote.quote_text.indexOf(` `) === -1)
+    quote_text = quote.quote_text;
+  else
+    quote_text = `"${quote.quote_text}"`;
+  message.reply(`${quote_text} - ${quote.username} (<@${quote.id}>)`);
 };
 
 module.exports.command = new Command(`RandomQuote`, DESCRIPTION, [], null, callback);
