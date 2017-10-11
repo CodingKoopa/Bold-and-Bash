b4ddaee07455ca850bd6c6150f3eaec85587d935
TheKoopaKingdom <thekoopakingdom@gmail.com>
2017-10-11 17:13:59 -0400

    Removed remaining message.mentions.users.forEach().

--- Source/Commands/ClearWarnings.js
+++ Source/Commands/ClearWarnings.js
@@ -12,28 +12,25 @@ const arg = [new Argument(`user`, `The user to clear warnings for.`, true, true)
 const roles = require(`../Common.js`).STAFF_ROLES;
 const callback = (message) =>
 {
-  // Use some and not forEach, so we can return.
-  message.mentions.users.forEach(user =>
-  {
-    const author_info = `${message.author.username} (${message.author})`;
-    const user_info = `${user.username} (${user})`;
-    const warnings = app.warnings.filter(x => x.id === user.id && !x.cleared);
-    common.SendPrivateInfoMessage(`${author_info} has cleared warnings for ${user_info} \
+  const user = message.mentions.users.first();
+  const author_info = `${message.author.username} (${message.author})`;
+  const user_info = `${user.username} (${user})`;
+  const warnings = app.warnings.filter(x => x.id === user.id && !x.cleared);
+  common.SendPrivateInfoMessage(`${author_info} has cleared warnings for ${user_info} \
 (${warnings.length} warnings).`);
 
-    message.reply(`clearing warnings for ${user_info}`);
+  message.reply(`clearing warnings for ${user_info}`);
 
-    if (warnings && warnings.length > 0)
-    {
-      warnings.forEach(warning => warning.cleared = true);
-      data.WriteWarnings();
-      message.channel.send(`${user}, your warnings have been cleared.`);
-    }
-    else
-    {
-      common.SendPrivateErrorMessage(`Failed to clear warnings for ${user_info}`);
-    }
-  });
+  if (warnings && warnings.length > 0)
+  {
+    warnings.forEach(warning => warning.cleared = true);
+    data.WriteWarnings();
+    message.channel.send(`${user}, your warnings have been cleared.`);
+  }
+  else
+  {
+    common.SendPrivateErrorMessage(`Failed to clear warnings for ${user_info}`);
+  }
 };
 
 module.exports.command = new Command(`ClearWarnings`, DESCRIPTION, arg, roles, callback);
