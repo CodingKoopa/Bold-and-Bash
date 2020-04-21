497a37bdea1d9234dc215954e68f389aa08ca66d
Koopa <codingkoopa@gmail.com>
2020-04-20 22:26:29 -0400

    Fix SC and EC issues.

--- CI/Deploy/Docker.sh
+++ CI/Deploy/Docker.sh
@@ -15,20 +15,20 @@ echo "Loading Docker image artifact."
 docker load --input Build/"$(CI/Common/GetTagShort.sh)".tar
 echo "Pushing Docker image for architecture \"$TARGET_ARCH\" to \"$TAG_LONG\"."
 # Push the Docker image to the registry.
-docker push $TAG_LONG
+docker push "$TAG_LONG"
 echo "Tagging Docker image \"$TAG_LONG\" as \"$TAG_LONG_LATEST\"."
 # Tag the Docker image as the latest image.
-docker tag $TAG_LONG $TAG_LONG_LATEST
+docker tag "$TAG_LONG" "$TAG_LONG_LATEST"
 echo "Pushing Docker image for architecture \"$TARGET_ARCH\" to \"$TAG_LONG_LATEST\" (Latest)."
 # Push the latest tag to the registry.
-docker push $TAG_LONG_LATEST
+docker push "$TAG_LONG_LATEST"
 # If a Git tag is present.
 if [ -n "$CI_COMMIT_TAG" ]; then
   TAG_LONG_STABLE=$CI_REGISTRY_IMAGE/$TARGET_ARCH:stable
   echo "Tagging Docker image \"$TAG_LONG\" as \"$TAG_LONG_STABLE\"."
   # Tag the Docker image as the latest stable image.
-  docker tag $TAG_LONG $TAG_LONG_STABLE
+  docker tag "$TAG_LONG" "$TAG_LONG_STABLE"
   echo "Pushing Docker image for architecture \"$TARGET_ARCH\" to \"$TAG_LONG_LATEST\" (Stable)."
   # Push the stable tag to the registry.
-  docker push $TAG_LONG_STABLE
+  docker push "$TAG_LONG_STABLE"
 fi
